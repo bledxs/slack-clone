76caf0188472444ab5484867698254033d04222c
Luis C Rojas <luisc.rojas@hotmail.com>
2024-08-31 01:30:58 -0500

    feat(auth): add authentication types
    Add the SignInFlow type to represent the different sign-in flows in the authentication feature.
    
    Co-authored-by: Create Next App

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  isAuthenticatedNextjs,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isSignInPage = createRouteMatcher(["/auth"]);
+
+export default convexAuthNextjsMiddleware((request) => {
+  if (!isSignInPage(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+
+  if(isSignInPage(request) && isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+
+  // TODO: Redirect user away from signin page if they are already signed in
+});
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
