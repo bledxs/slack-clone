64620bc1d496d0a6828e20573825b44a5bc3f083
Luis C Rojas <luisc.rojas@hotmail.com>
2024-09-01 19:09:34 -0500

    refactor: optimize workspace section component and add collapsible functionality

--- src/app/workspace/[workspaceId]/workspace-section.tsx
+++ src/app/workspace/[workspaceId]/workspace-section.tsx
@@ -1,7 +1,9 @@
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { PlusCircleIcon } from "lucide-react";
 import { FaCaretDown } from "react-icons/fa";
+import { useToggle } from "react-use";
 
 type WorkspaceSectionProps = {
   label: string;
@@ -15,14 +17,19 @@ export const WorkspaceSection = ({
   label,
   onNew,
 }: WorkspaceSectionProps) => {
+  const [on, toggle] = useToggle(true);
+
   return (
     <div className="flex flex-col mt-3 px-2">
       <div className="flex items-center px-3.5 group">
         <Button
           variant="transparent"
           className="p-0.5 text-sm text-[#f9edffcc] shrink-0 size-6"
+          onClick={toggle}
         >
-          <FaCaretDown className="size-4" />
+          <FaCaretDown
+            className={cn("size-4 transition-transform", on && "-rotate-90")}
+          />
         </Button>
         <Button
           variant="transparent"
@@ -44,7 +51,7 @@ export const WorkspaceSection = ({
           </Hint>
         )}
       </div>
-      {children}
+      {on && children}
     </div>
   );
 };
