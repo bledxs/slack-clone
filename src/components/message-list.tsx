3d6dfb34b979a84aebff1882f2a6c836bfafe022
Luis C. Rojas <luis.rojas@itglobers.com>
2025-01-27 11:38:30 -0500

    feat: add load more messages functionality

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Id } from "../../convex/_generated/dataModel";
 import ChannelHero from "./channel-hero";
 import { Message } from "./message";
+import { Loader } from "lucide-react";
 
 const TIME_THRESHOLD = 5;
 
@@ -106,6 +107,31 @@ export const MessageList = ({
           </div>
         </div>
       ))}
+      <div
+        className="h-1"
+        ref={(el) => {
+          if (!el || !canLoadMore) return;
+          const observer = new IntersectionObserver(
+            (entries) => {
+              if (entries[0].isIntersecting) {
+                loadMore();
+              }
+            },
+            { threshold: 1 }
+          );
+          observer.observe(el);
+          return () => observer.disconnect();
+        }}
+      />
+
+      {isLoadingMore && (
+        <div className="text-center my-2 relative">
+          <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+          <span className="relative inline-block bg-white px-4 py-1 rounded-full text-sm border border-gray-300 shadow-sm">
+            <Loader className="size-4 animate-spin" />
+          </span>
+        </div>
+      )}
       {variant === "channel" && channelName && channelCreationTime && (
         <ChannelHero name={channelName} creationTime={channelCreationTime} />
       )}
